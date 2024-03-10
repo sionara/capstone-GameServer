1dd94b30c18800ffba7d8f2eeb1ab7a254544376
Sion Lee <35111239+sionara@users.noreply.github.com>
2024-03-10 19:59:32 -0400

    Moved client files to another repo and renamed this remote repo URL

--- server/index.js
+++ server/index.js
@@ -18,12 +18,24 @@ const io = new Server(server, {
     },
 });
 
+//detects connections from clients
 io.on("connection", (socket) => {
     console.log(`user connected: ${socket.id}`);
-    //receives user_choice value
-    socket.on("user_choice", (data) => {
+    
+    //creates and joins the room with roomId
+    socket.on("join_room", (roomId) => {
+        socket.join(roomId);
+    });
+
+    //receives user_choice value. here should be logic to check if theres a winner
+    socket.on("user_choice", (data ) => { // this data should contain what hand user dealt, and room id
         console.log(data);
-    } )
+        // here we should emit a message to winner and loser separately
+        socket.to(data.roomId).emit("You Won!");
+    } );
+
+    
+    
 })
 
 server.listen(3001, () => {
